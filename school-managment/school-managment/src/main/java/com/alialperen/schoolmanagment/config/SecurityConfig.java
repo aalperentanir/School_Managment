80bf0b1fabf5123e9afc6636e584df3aaa1fd913
Ali Alperen TANIR <alialperentanir@gmail.com>
2024-04-23 12:26:53 +0300

    backend

---
+++ school-managment/school-managment/src/main/java/com/alialperen/schoolmanagment/config/SecurityConfig.java
@@ -0,0 +1,81 @@
+package com.alialperen.schoolmanagment.config;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import com.alialperen.schoolmanagment.filter.JwtRequestFilter;
+
+import lombok.RequiredArgsConstructor;
+
+
+@Configuration
+@EnableMethodSecurity
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+	
+	
+	private final JwtRequestFilter jwtfilter;
+	@Bean
+    public SecurityFilterChain securityConfiguration(HttpSecurity http) throws Exception {
+		return http.cors().configurationSource(corsConfigurationSource()).and()
+            .csrf().disable()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+                .and().authorizeHttpRequests().requestMatchers("/api/**").authenticated()
+                .anyRequest().authenticated().and()
+                .addFilterBefore(jwtfilter, UsernamePasswordAuthenticationFilter.class).build();
+		
+		/*return http
+            .csrf().disable().authorizeHttpRequests()
+                .requestMatchers(HttpMethod.POST, "/authenticate").permitAll()
+                .and().authorizeHttpRequests().requestMatchers("/api/**").authenticated()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+                .addFilterBefore(jwtfilter, UsernamePasswordAuthenticationFilter.class).build();*/
+
+    }
+	
+	@Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200")); // İzin verilen kökenler
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE")); // İzin verilen HTTP metodları
+        configuration.setAllowedHeaders(Arrays.asList("*"));
+        configuration.addExposedHeader("Authorization");// İzin verilen başlıklar
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+	
+	@Bean
+	public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+		return config.getAuthenticationManager();
+	}
+
+}
